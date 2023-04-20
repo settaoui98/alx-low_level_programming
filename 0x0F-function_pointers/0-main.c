5d0e8b37716a484e1a9c49ee71ca846583560435
settaoui98 <hamzasitaoui@gmail.com>
2023-04-19 19:00:15 -0700

    Bob

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "function_pointers.h"
 
 /**
  * print_name_as_is - prints a name as is
@@ -44,6 +45,8 @@ void print_name_uppercase(char *name)
  */
 int main(void)
 {
-    
+    print_name("Bob", print_name_as_is);
+    print_name("Bob Dylan", print_name_uppercase);
+    printf("\n");
     return (0);
 }
